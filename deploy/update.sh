3da4f19abd9b79fbbd09c4ded9884a21752dae37
VigneshKrishnamoorthy112 <kvignesh112@gmail.com>
2022-02-26 12:35:21 +0530

    Update update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -3,9 +3,9 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/VigneshKrishnamoorthy112/django_hosting.git'
+PROJECT_GIT_URL='git@github.com:VigneshKrishnamoorthy112/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/django_hosting'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
